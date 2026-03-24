03478cd07d68af86e84535084a13879c18ab9a8d
tinhne <levantrungtinh13@gmail.com>
2026-03-24 19:24:33 +0700

    20: Meeting page

---
+++ src/modules/meetings/ui/components/update-meeting-dialog.tsx
@@ -0,0 +1,30 @@
+import { ResponsiveDialog } from "@/components/responsive-dialog";
+import { MeetingForm } from "./meeting-form";
+import { MeetingGetOne } from "../../types";
+
+interface UpdateMeetingDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  initialValues: MeetingGetOne;
+}
+
+export const UpdateMeetingDialog = ({
+  open,
+  onOpenChange,
+  initialValues,
+}: UpdateMeetingDialogProps) => {
+  return (
+    <ResponsiveDialog
+      open={open}
+      onOpenChange={onOpenChange}
+      title="Update Meeting"
+      description="Update the details of this meeting"
+    >
+      <MeetingForm
+        onSuccess={() => onOpenChange(false)}
+        onCancel={() => onOpenChange(false)}
+        initialValues={initialValues}
+      />
+    </ResponsiveDialog>
+  );
+};
